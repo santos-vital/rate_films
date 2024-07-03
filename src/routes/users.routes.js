e4b1ff0b54d0a08f3e044db17da94b638c304e3a
santos-vital <vital.leandro16@gmail.com>
2024-07-03 17:40:54 -0300

    Feat: Atualizar avatar

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,19 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../config/upload");
 
 const UsersController = require("../controllers/UsersController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+const UsersAvatarController = require("../controllers/UsersAvatarController");
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController();
+const usersAvatarController = new UsersAvatarController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), usersAvatarController.update);
 
 module.exports = usersRoutes;
